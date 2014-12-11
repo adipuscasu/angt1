f9da6558aeffc4acae85de73e1f99b1a4abf857c
Adi <Adi@GreenTea>
2014-12-12 00:41:29 +0200

    Merge origin/master
    Conflicts:
    	app/models/firma.server.model.js

---
+++ public/modules/firme/services/firme.client.service.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//Firme service used for communicating with the firme REST endpoints
+angular.module('firme').factory('Firme', ['$resource',
+	function($resource) {
+		return $resource('firme/:articleId', {
+			firmaId: '@_id'
+		}, {
+			update: {
+				method: 'PUT'
+			}
+		});
+	}
+]);
