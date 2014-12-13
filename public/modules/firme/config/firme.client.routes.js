af036bb6d95a3fef4cc909a2122f2d56d70ffffd
Adi <Adi@GreenTea>
2014-12-13 22:47:29 +0200

    Merge origin/master
    Conflicts:
    	app/models/firma.server.model.js

--- public/modules/firme/config/firme.client.routes.js
+++ public/modules/firme/config/firme.client.routes.js
@@ -11,15 +11,15 @@ angular.module('firme').config(['$stateProvider',
 		}).
 		state('createFirme', {
 			url: '/firme/create',
-			templateUrl: 'modules/firme/views/create-article.client.view.html'
+			templateUrl: 'modules/firme/views/create-firma.client.view.html'
 		}).
 		state('viewFirme', {
-			url: '/firme/:articleId',
-			templateUrl: 'modules/firme/views/view-article.client.view.html'
+			url: '/firme/:firmaId',
+			templateUrl: 'modules/firme/views/view-firma.client.view.html'
 		}).
 		state('editFirme', {
-			url: '/firme/:articleId/edit',
-			templateUrl: 'modules/firme/views/edit-article.client.view.html'
+			url: '/firme/:firmaId/edit',
+			templateUrl: 'modules/firme/views/edit-firma.client.view.html'
 		});
 	}
 ]);
