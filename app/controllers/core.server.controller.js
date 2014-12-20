406ac950987bc51a58f490579c9325eab3434ea1
Adi <Adi@GreenTea>
2014-12-20 23:51:56 +0200

    actualizare

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -3,15 +3,9 @@
 /**
  * Module dependencies.
  */
-//exports.index = function(req, res) {
-//	res.render('index', {
-		//user: req.user || null,
-		//request: req
-	//});
-//};
 exports.index = function(req, res) {
-	res.render('/firme', {
-		user: req.user ,
+	res.render('index', {
+		user: req.user || null,
 		request: req
 	});
 };
