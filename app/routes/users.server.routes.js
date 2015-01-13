6da6606e666d3c8d65bdef651f7d03bbeb228adc
Adi <Adi@GreenTea>
2015-01-13 22:44:27 +0200

    Merge origin/master
    Conflicts:
    	app/models/firma.server.model.js

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
 
 	// Setting up the users profile api
 	app.route('/users/me').get(users.me);
-        app.route('/users/list').get(users.list);
+        app.route('/users/list').get(users.requiresLogin, users.list);
 	app.route('/users/update').put(users.update);
 	app.route('/users/create').post(users.create);
 	app.route('/users/accounts').delete(users.removeOAuthProvider);
